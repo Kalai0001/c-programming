6c2c10f943100f0ff21ceb4a067033bc574a6e7e
Kalai0001 <kalai1172000@gmail.com>
2026-02-03 22:29:23 +0530

    Add files via upload

---
+++ Nested_loops/nested19.c
@@ -0,0 +1,32 @@
+#include<stdio.h>
+int main(){
+    int n;
+    scanf("%d",&n);
+    for(int i=n;i>=1;i--){
+        for(int j=1;j<=i;j++){
+            printf("*");
+        }
+        for(int space=1;space<=2*(n-i);space++){
+            printf(" ");
+        }
+        for(int k=1;k<=i;k++){
+            printf("*");
+        }
+        
+        printf("\n");
+    }
+
+    for(int i=1;i<=n;i++){
+        for(int j=1;j<=i;j++){
+            printf("*");
+        }
+        for(int space=1;space<=n-i;space++){
+            printf("  ");
+        }
+        for(int k=1;k<=i;k++){
+            printf("*");
+        }
+        printf("\n");
+
+    }
+}
