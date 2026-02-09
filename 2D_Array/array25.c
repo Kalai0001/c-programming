28964ffa86372420b85c80fd494d28a5480d60b4
Kalai0001 <kalai1172000@gmail.com>
2026-02-09 16:18:28 +0530

    Add files via upload

---
+++ 2D_Array/array25.c
@@ -0,0 +1,32 @@
+#include<stdio.h>
+int main(){
+    int r,c;
+    scanf("%d %d",&r,&c);
+    int arr[r][c];
+    for(int i=0;i<r;i++){
+        for(int j=0;j<c;j++){
+            scanf("%d",&arr[i][j]);
+        }
+    }
+    int count=0;
+    for(int i=0;i<r;i++){
+        int max=0;
+        int min=100000000;
+        for(int j=0;j<c;j++){
+            if(arr[i][j]>max){
+                max=arr[i][j];
+            }
+            if(arr[i][j]<min){
+                min=arr[i][j];
+            }
+        }
+        int z=max-min;
+        if(z<=1000){
+            count++;
+        }
+        printf("max: %d\n",max);
+        printf("min: %d\n",min);
+    }
+    printf("%d",count);
+}
+    
