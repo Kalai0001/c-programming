92a8f238ce7b7515b7fe412c72da1820ede16637
Kalai0001 <kalai1172000@gmail.com>
2026-02-10 07:24:40 +0530

    Add files via upload

---
+++ 2D_Array/array15.c
@@ -0,0 +1,29 @@
+#include<stdio.h>
+int main(){
+    int r,c;
+    scanf("%d %d",&r,&c);
+    int arr[r][c];
+    for(int i=0;i<r;i++){
+        for(int j=0;j<c;j++){
+            scanf("%d",&arr[i][j]);
+        }
+    }
+    int n=0;
+    int count=0;
+    for(int i=0;i<r;i++){
+        for(int j=0;j<c;j++){
+            n=arr[i][j];
+            int flag=0;
+            for(int k=2;k<n;k++){
+                if(n%k==0){
+                    flag=1;
+                    break;
+                }
+            }
+            if(flag==0){
+                count++;
+            }
+        }
+    }
+    printf("%d",count);
+}
